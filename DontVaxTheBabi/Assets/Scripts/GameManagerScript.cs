5e22598b1401584ce2fa964c3fe29125a9fd5e2b
lip ables <phillipables1@gmail.com>
2019-01-05 06:17:42 -0500

    game manager script has public reference for baby script and needle script, linked prefabs to reference in game view

--- DontVaxTheBabi/Assets/Scripts/GameManagerScript.cs
+++ DontVaxTheBabi/Assets/Scripts/GameManagerScript.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour {
+    public BabyScript baby;
+    public NeedleScript needle;
 
 	// Use this for initialization
 	void Start () {
